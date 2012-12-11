af028b51c10874bd3f319172a0ca48ed11da08f7
Marcelo Ramos <marcelo@clk.comm.uy>
2012-12-11 12:13:08 -0200

    Fix view call

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^info/$', 'djangoinfo.views.info', name='info'),
+    url(r'^info/$', 'django_info.views.info', name='info'),
 )
