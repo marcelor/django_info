dc42be97fa32390e1342eae45a949e279ba9f11f
juanpex <jpma55@gmail.com>
2013-06-28 13:31:42 -0700

    Update urls.py
    fix for 1.6

--- django_info/urls.py
+++ django_info/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls.defaults import *
+except ImportError:
+    from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^info/$', 'django_info.views.info', name='info'),
